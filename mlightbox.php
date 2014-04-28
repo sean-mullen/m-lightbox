30b2f13c0ff21187ab24590c1ca3a36a5482e51b
Sean Mullen <sean@mullen.net.au>
2014-04-28 15:19:15 +1000

    first commit

---
+++ mlightbox.php
@@ -0,0 +1,99 @@
+<?php
+
+/*
+Plugin Name: M:Lightbox
+Plugin URI: http://www.mullen.net.au/
+Description: An unobstrusive image lightbox that requires no configuration.
+Version: 1.0.0
+Author: Sean Mullen <sean@mullen.net.au>
+Author URI: http://www.mullen.net.au/
+License: MIT
+*/
+
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Sean Mullen <sean@mullen.net.au>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+class mlightbox {
+
+	const VERSION = '1.0.0';
+	protected $plugin_slug = 'mlightbox';
+	protected static $instance = null;
+	private function __construct() {
+		// Load public-facing style sheet and JavaScript.
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+	}
+
+	/**
+	 * Return the plugin slug.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @return    Plugin slug variable.
+	 */
+	public function get_plugin_slug() {
+		return $this->plugin_slug;
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @since     1.0.0
+	 *
+	 * @return    object    A single instance of this class.
+	 */
+	public static function get_instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+
+	/**
+	 * Register and enqueue public-facing style sheet.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+		wp_enqueue_style( $this->plugin_slug . '-plugin-styles', plugins_url( 'css/mlightbox.css', __FILE__ ), array(), self::VERSION );
+	}
+
+	/**
+	 * Register and enqueues public-facing JavaScript files.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+		wp_enqueue_script( $this->plugin_slug . '-plugin-script', plugins_url( 'js/imagelightbox.js', __FILE__ ), array( 'jquery' ), self::VERSION );
+	}
+
+
+}
+add_action( 'plugins_loaded', array( 'mlightbox', 'get_instance' ) );
+
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Plugin Name ===
+Contributors: Sean Mullen <sean@mullen.net.au>
+Donate link: 
+Tags: 
+Requires at least: 
+Tested up to: 
+Stable tag: 
+License: MIT
+License URI: http://opensource.org/licenses/MIT
+
+
+== Description ==
+m:lightbox is a modern, minimal lightbox. Insert links to images/image attachments into posts and pages and the image will be displayed in a lightbox automatically.**
+
+Features:
+* Fully-responsive image lightbox
+* No configurable options
+* No admin links
+* No pro version
+
+This lightbox plugin is wraps imagelightbox (www.osvaldas.info) by Osvaldas Valutis into a plugin
+
+== Installation ==
+1. Install and activate
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
